61893627680168e158fda6ef003d199b575716a1
satoruakefu820-dev <satoruakefu820@gmail.com>
2026-06-04 10:02:11 +0500

    check_tables.php

--- check_tables.php
+++ check_tables.php
@@ -1,11 +1,19 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include "db_connect.php";
 
 $result = $conn->query("SHOW TABLES");
+
+if(!$result){
+    die($conn->error);
+}
 
 while($row = $result->fetch_array()){
     echo $row[0] . "<br>";
 }
 
+echo "END";
 ?>
