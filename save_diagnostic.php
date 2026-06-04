64850715b6232ff77ab8c6c6247f896f2e9a7c56
satoruakefu820-dev <satoruakefu820@gmail.com>
2026-06-04 10:14:20 +0500

    save_diagnostic.php

--- save_diagnostic.php
+++ save_diagnostic.php
@@ -1,5 +1,45 @@
 <?php
 
+include "db_connect.php";
+
+if (
+    isset($_POST["patient_id"]) &&
+    isset($_POST["fullname"]) &&
+    isset($_POST["temperature"]) &&
+    isset($_POST["pressure"]) &&
+    isset($_POST["sugar"]) &&
+    isset($_POST["diagnosis"])
+) {
+
+    $patient_id = $_POST["patient_id"];
+    $fullname = $_POST["fullname"];
+    $temperature = $_POST["temperature"];
+    $pressure = $_POST["pressure"];
+    $sugar = $_POST["sugar"];
+    $diagnosis = $_POST["diagnosis"];
+
+    $sql = "INSERT INTO diagnostics
+    (patient_id, fullname, temperature, pressure, sugar, diagnosis)
+    VALUES
+    ('$patient_id','$fullname','$temperature','$pressure','$sugar','$diagnosis')";
+
+    if ($conn->query($sql)) {
+        echo "success";
+    } else {
+        echo $conn->error;
+    }
+
+} else {
+
+    echo "Waiting for data...";
+
+}
+
+$conn->close();
+?>
+
+<?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
