d3df0a97f20fbd75e5f4abdfd6052b9c74681790
satoruakefu820-dev <satoruakefu820@gmail.com>
2026-06-09 00:10:29 +0500

    Update save_to_doctor.php

--- save_to_doctor.php
+++ save_to_doctor.php
@@ -8,13 +8,22 @@ $doctor = $_POST["doctor"];
 $date = $_POST["date"];
 $time = $_POST["time"];
 
-$sql = "INSERT INTO doctors_queue 
+$sql = "INSERT INTO doctors_queue
 (patient_id, fullname, doctor, visit_date, visit_time)
 VALUES
 ('$patient_id','$fullname','$doctor','$date','$time')";
 
 if($conn->query($sql)){
+
+    // Увеличиваем количество пациентов у врача
+    $conn->query("
+        UPDATE doctors
+        SET patients_count = patients_count + 1
+        WHERE doctor_name = '$doctor'
+    ");
+
     echo "success";
+
 }else{
     echo $conn->error;
 }
